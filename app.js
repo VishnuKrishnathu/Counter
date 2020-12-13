6a29a5e728dcd7206db050d60cbbd0152269087b
VishnuKrishnathu <vishnu.krishnathu1999@gmail.comgit config --global user.email vishnu.krishnathu1999@gmail.com>
2020-12-13 18:25:35 +0530

    modified

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const buttons = document.querySelectorAll("#btn");
-console.log(buttons);
+console.log(buttons[1].attributes[0].value);
+buttons.forEach((button) => (
+    button.addEventListener("click",() => {
+        let count = 0;
+        if (button.attributes[0].value === "decrease"){
+            console.log(123);
+        }
+    } )
+))

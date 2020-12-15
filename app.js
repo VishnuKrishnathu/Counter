949e3d758108c412787628e119cf6669de8e205c
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-12-15 13:43:37 +0530

    Timer added for decrement

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var count = 0;
 buttons.forEach((button) => (
     button.addEventListener("click",async () => {
         if (button.attributes[0].value === "decrease"){
-            await decrement(count);
+            await decrement(count)
             }
         }
  ) )
 )
 function decrement(count){
     new Promise((resolve,reject) => {
-        setTimeout(() => {
-            count++
-            document.querySelector("#num").textContent = count
-        }, 1000)
+        clearInterval(interval);
+        var interval =setInterval(() => {
+                count--
+                resolve(document.querySelector("#num").textContent = count)
+            }, 1000)
     })
 }

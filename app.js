967a491a5b2ac2393e2ffa89c50e87be6583fddb
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-12-15 12:00:12 +0530

    modified

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const buttons = document.querySelectorAll("#btn");
 console.log(buttons[1].attributes[0].value);
+var count = 0;
 buttons.forEach((button) => (
     button.addEventListener("click",async () => {
-        let count = 0;
         if (button.attributes[0].value === "decrease"){
-            while(true){
-                count++
-                await setTimeout(decrement(count), 1000)
-                
+            await decrement(count);
             }
         }
-    } )
-))
+ ) )
+)
 function decrement(count){
-    document.querySelector("#num").textContent = count
+    new Promise((resolve,reject) => {
+        setTimeout(() => {
+            count++
+            document.querySelector("#num").textContent = count
+        }, 1000)
+    })
 }

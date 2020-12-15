603bd643304a87545dfa116d358662ddda712db9
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-12-15 10:55:14 +0530

    modified

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const buttons = document.querySelectorAll("#btn");
 console.log(buttons[1].attributes[0].value);
 buttons.forEach((button) => (
-    button.addEventListener("click",() => {
+    button.addEventListener("click",async () => {
         let count = 0;
         if (button.attributes[0].value === "decrease"){
-            console.log(123);
+            while(true){
+                count++
+                await setTimeout(decrement(count), 1000)
+                
+            }
         }
     } )
 ))
+function decrement(count){
+    document.querySelector("#num").textContent = count
+}

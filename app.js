a2c2347302bd6cc579c7a8039f0a791c84330062
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2020-12-21 22:03:19 +0530

    finished

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const buttons = document.querySelectorAll("#btn");
 console.log(buttons[1].attributes[0].value);
-var interv;
-var stopper = true;
+let interv;
+let stopper = true;
 var count = 0;
 buttons.forEach((button) => (
     button.addEventListener("click",async () => {
         if (button.attributes[0].value === "decrease"){
             await decrement(count)
             } else if (button.attributes[0].value === "reset"){
+                stopper = true;
                 clearInterval(interv);
                 document.querySelector("#num").textContent = 0;
+            }else{
+                await increament(count)
             }
         }
  ) )
@@ -19,7 +22,21 @@ function decrement(count){
         if (stopper){
             stopper = false;
             interv =setInterval(() => {
-                count--
+                --count;
+                resolve(document.querySelector("#num").textContent = count)
+            }, 1000)
+        }else {
+            stopper = true;
+            clearInterval(interv);
+        }
+    })
+}
+function increament(count){
+    new Promise((resolve,reject) => {
+        if (stopper){
+            stopper = false;
+            interv =setInterval(() => {
+                ++count;
                 resolve(document.querySelector("#num").textContent = count)
             }, 1000)
         }else {
